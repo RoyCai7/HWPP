578c6012bcbd43584086dd899199d96cfcaa5d67
Roy Cai <roy.cai@suse.com>
2022-06-27 14:00:56 +0800

    Developing

--- suseautolab/views.py
+++ suseautolab/views.py
@@ -1,26 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
- 
+
+
 def hello(request):
     return HttpResponse("Hello world ! ")
 
+
 def runoob(request):
-    context          = {}
+    context = {}
     context['hello'] = 'Hello World!'
     return render(request, 'runoob.html', context)
+
 
 def search_form(request):
     return render(request, 'search_form.html')
 
+
 def search_result(request):
-    request.encoding='utf-8'
+    request.encoding = 'utf-8'
     if 'q' in request.GET and request.GET['q']:
         message = 'the content of your input: ' + request.GET['q']
     else:
-        message = 'nothing' 
-    #return HttpResponse(message)
+        message = 'nothing'
+    # return HttpResponse(message)
+    context = {}
+    context['message'] = message
     return render(request, 'search_result.html', message)
+
 
 def search_module(request):
     return render(request, 'search_module.html')
-
